9535fde9dc73c6ece5505fd541d7eaf374161c2c
Tiberiu Ioan <tiberiuioan35@gmail.com>
2021-05-04 10:33:36 +0300

    :bug: Bugfix

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -9,35 +9,37 @@ const Articles = ({ data }: Props) => {
     <div className="article">
       <h1>NEWS</h1>
 
-      {data.articles.map(
-        (
-          article: {
-            title: string;
-            author: string;
-            publishedAt: string;
-            description: string;
-            content: string;
-            url: string;
-          },
-          index: number
-        ) => {
-          let date: Date = new Date(article.publishedAt);
+      {data.status && data.status === "ok"
+        ? data.articles.map(
+            (
+              article: {
+                title: string;
+                author: string;
+                publishedAt: string;
+                description: string;
+                content: string;
+                url: string;
+              },
+              index: number
+            ) => {
+              let date: Date = new Date(article.publishedAt);
 
-          return (
-            <ArticleDesc
-              title={article.title}
-              author={article.author}
-              publishedAt={`${date.getDate()}.${
-                date.getMonth() + 1
-              }.${date.getFullYear()}`}
-              description={article.description}
-              content={article.content}
-              url={article.url}
-              key={index}
-            />
-          );
-        }
-      )}
+              return (
+                <ArticleDesc
+                  title={article.title}
+                  author={article.author}
+                  publishedAt={`${date.getDate()}.${
+                    date.getMonth() + 1
+                  }.${date.getFullYear()}`}
+                  description={article.description}
+                  content={article.content}
+                  url={article.url}
+                  key={index}
+                />
+              );
+            }
+          )
+        : null}
       <style jsx>{`
         h1 {
           margin: 50px auto 0 auto;
